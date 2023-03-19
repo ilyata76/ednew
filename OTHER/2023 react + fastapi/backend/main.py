588a7d481916ccd45c671130ad6c012198fe078a
ilyata <ilyata76@yandex.ru>
2023-03-20 00:06:01 +0300

    hello world from api to page

--- OTHER/2023 react + fastapi/backend/main.py
+++ OTHER/2023 react + fastapi/backend/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:8000"
+] # разрешит связь с этими адресами
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Hello World"}
--- OTHER/2023 react + fastapi/frontend/src/App.jsx
+++ OTHER/2023 react + fastapi/frontend/src/App.jsx
@@ -1,10 +1,35 @@
 // jsx - js внутри html блоков
 
+import React, {useState, useEffect} from "react";
 
 const App = () => {
+  const [message, setMessage] = useState(""); // react фича, храним состояние
+
+  const getWelcomeMessage = async () => {
+    const requestOptions = {
+      method: "GET",
+      headers : {
+        "Content-Type": "application/json"
+      }
+    };
+    const response = await fetch('/api', requestOptions);
+
+    if (!response.ok) {
+      console.log(response.status);
+    } else {
+      const data = await response.json();
+      setMessage(data.message);
+    }
+  };
+
+  useEffect(() => {
+    getWelcomeMessage();
+  }, []); // ещё один react hook
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>{message}</h1>
+      <p>ура</p>
     </div>
   );
 }
