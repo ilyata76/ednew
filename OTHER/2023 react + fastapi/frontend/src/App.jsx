2887a4d2008dc3f6ece67ed498d81adfe354f639
ilyata <ilyata76@yandex.ru>
2023-03-20 16:12:19 +0300

    navbar

--- OTHER/2023 react + fastapi/frontend/src/App.jsx
+++ OTHER/2023 react + fastapi/frontend/src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
+import Root from "./Root"
 import Home from "./Home"
+import NavBar from "./NavBar";
 
 const App = () => {
   return (
     <div>
+    
+      <p>То, что меняться не будет</p>
+      
+
+      
       <Router>
-      <Routes>
-        <Route path='/home' element={<Home/>} />
-      </Routes>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Root/>}/>
+          <Route path='/home' element={<Home/>} />
+        </Routes>
       </Router>
+
     </div>
   );
 }
